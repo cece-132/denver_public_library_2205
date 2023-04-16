9f726f6138f88a33550ba80049ab9d39c057b76f
Candace Eckels <candace.eckels@gmail.com>
2023-04-15 22:44:42 -0600

    updated code

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,8 @@
 require 'pry'
 
 class Book
-    attr_reader :author_first_name, :author_last_name, :title, :publication_date
+    attr_reader :author_first_name, :author_last_name, 
+                :title, :publication_date
     def initialize(data)
         @author_first_name = data[:author_first_name] 
         @author_last_name = data[:author_last_name]
